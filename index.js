538cb3328d88be139d60a8990bcad9ded2a8e11c
Alex Lee <hyunho98@gmail.com>
2019-05-03 22:40:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
+
+function nowServing(katzDeliLine) {
+  var current = katzDeliLine[0];
+  katzDeliLine.unshift();
+  return current;
+}
